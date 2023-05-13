e721ffc2e26ce7efa3cc155e1aed94cabeec2b09
Ahmed <ahmelsamahy@gmail.com>
2023-05-13 18:40:17 +0200

    feat<>: the file is working as it should now
    it generates a folder for each training session with the date being the name of it. The folder will have the .CSV files collected from the compressed files along with a filtered .CSV that have only x_coord, y_coord and vehicle_conf columns. 
    It .GPX and .HTML file for each training session but in different folder. the first one is in the main root and the second one is in '/training_maps' folder

--- teleop/htm/plot_training_sessions_map/draw_training_sessions.py
+++ teleop/htm/plot_training_sessions_map/draw_training_sessions.py
@@ -9,17 +9,6 @@ import pandas as pd
 import pathlib
 
 
-# To store the name of the day the session took place in it.
-SESSION_DATE = ""
-
-# The Resultant file with all the rows from a training session made in a day
-MERGED_CSV_FILES = f"resultant_{SESSION_DATE}.csv"
-
-GPX_FILE = f"{SESSION_DATE}.gpx"
-
-# Main list to have all the .CSV files as their own list in the training session folder
-L = []
-
 # Filtered columns that will be used in the resultant .CSV file.
 KEEP_COL = ["x_coord", "y_coord", "vehicle_conf"]
 
@@ -28,10 +17,14 @@ TRAINING_SESSION_LOCATION = "D:/Job/Byodr/__myStuff/routeAccu/trainingSession/"
 # Get all the compressed files in a folder
 ZIP_FILES_LOCATION = glob.glob(f"{TRAINING_SESSION_LOCATION}**/*.zip", recursive=True)
 
+# To store the name of the day the session took place in it.
+SESSION_DATE = []
+
+# List for the location of the resultant .CSVs made in a day.
+MERGED_CSV_FILES_LOCATION = []
+
 # Store the folder of sessions
 CSV_FILES_LOCATION = []
-
-TRAINING_SESSIONS_DATE = []
 
 
 class FindCSV:
@@ -40,28 +33,35 @@ class FindCSV:
     """
 
     def store_sessions_folder(self):
-        global MERGED_CSV_FILES, GPX_FILE
+        global SESSION_DATE
         """store in "CSV_FILES_LOCATION" the location for the training sessions 
         where the .CSV files are moved from the compressed file of training sessions
         """
         if len(CSV_FILES_LOCATION) == 0:
             CSV_FILES_LOCATION.append(
-                "{0}\\{1}".format(pathlib.Path(__file__).parent.resolve(), SESSION_DATE)
+                "{0}\\{1}".format(
+                    pathlib.Path(__file__).parent.resolve(), SESSION_DATE[-1]
+                )
             )
-        elif not SESSION_DATE in CSV_FILES_LOCATION[-1]:
+        elif not SESSION_DATE[-1] in CSV_FILES_LOCATION[-1]:
             CSV_FILES_LOCATION.append(
-                "{0}\\{1}".format(pathlib.Path(__file__).parent.resolve(), SESSION_DATE)
+                "{0}\\{1}".format(
+                    pathlib.Path(__file__).parent.resolve(), SESSION_DATE[-1]
+                )
             )
-        MERGED_CSV_FILES = f"resultant_{SESSION_DATE}.csv"
-        GPX_FILE = f"{SESSION_DATE}.gpx"
 
     def extract_files_by_extension(self):
         global SESSION_DATE
         for file in ZIP_FILES_LOCATION:
             # To get the date of session only, from the CSV file
-            SESSION_DATE = os.path.basename(file)[:9]
+            if len(SESSION_DATE) == 0:
+                SESSION_DATE.append(os.path.basename(file)[:9])
+            elif not os.path.basename(file)[:9] in SESSION_DATE[-1]:
+                SESSION_DATE.append(os.path.basename(file)[:9])
 
-            self.create_sessions_folder()
+            # SESSION_DATE = os.path.basename(file)[:9]
+
+            self.create_sessions_folder(SESSION_DATE[-1])
 
             self.store_sessions_folder()
 
@@ -69,14 +69,18 @@ class FindCSV:
                 # Get all the files in the chosen compressed file
                 for file_info in zip_ref.infolist():
                     if file_info.filename.endswith(".csv"):
-                        extracted_path = zip_ref.extract(file_info, path=SESSION_DATE)
-                        new_file_path = os.path.join(SESSION_DATE, file_info.filename)
+                        extracted_path = zip_ref.extract(
+                            file_info, path=SESSION_DATE[-1]
+                        )
+                        new_file_path = os.path.join(
+                            SESSION_DATE[-1], file_info.filename
+                        )
                         os.rename(extracted_path, new_file_path)
 
-    def create_sessions_folder(self):
+    def create_sessions_folder(self, session_data):
         """Create a folder with the date of the sessions"""
-        if not os.path.exists(SESSION_DATE):
-            os.makedirs(SESSION_DATE)
+        if not os.path.exists(session_data):
+            os.makedirs(session_data)
 
 
 class ProcessCSVtoGPX:
@@ -84,25 +88,27 @@ class ProcessCSVtoGPX:
 
     def __init__(self):
         self.df = ""
+        self.merged_csv_files = ""
+        # Main list to have all the .CSV files as their own list in the training session folder
+        self.dataframe_data = []
 
     def create_resultant_CSV(self):
         """Create a main .CSV file that will have data from all the .CSV that are founded in a training session."""
 
         # keep only the coordinates columns in them
-        for folder in CSV_FILES_LOCATION:
-            L.clear()
-
-            CSV_files = glob.glob(f"{folder}\\*.csv")
+        for folder in zip(CSV_FILES_LOCATION, SESSION_DATE):
+            self.dataframe_data.clear()
+            CSV_files = glob.glob(f"{folder[0]}\\*.csv")
             for file in CSV_files:
-                # Read every file in the folder
+                # Read every .CSV file in the folder
                 read_file = pd.read_csv(file)
                 new_file = read_file[KEEP_COL]
-                L.append(new_file)
+                self.dataframe_data.append(new_file)
 
-            self.df = pd.concat(L, ignore_index=True)
+            self.df = pd.concat(self.dataframe_data, ignore_index=True)
             self.clean_dataframe()
-            self.convert_to_CSV()
-            self.convert_to_GPX()
+            self.convert_to_CSV(folder[1])
+            self.convert_to_GPX(folder[1])
 
     def clean_dataframe(self):
         """Remove duplication from the table."""
@@ -112,34 +118,24 @@ class ProcessCSVtoGPX:
             f"Removed the duplicates in the data from {old_count} to {self.df.shape[0]}"
         )
 
-    def convert_to_CSV(self):
-        self.df.to_csv(MERGED_CSV_FILES, index=False)
-        print(f"CSV file saved as {MERGED_CSV_FILES}")
+    def convert_to_CSV(self, session_data):
+        self.merged_csv_files = f"{session_data}\\resultant_{session_data}.csv"
+        MERGED_CSV_FILES_LOCATION.append(os.path.abspath(self.merged_csv_files))
+        self.df.to_csv(self.merged_csv_files, index=False)
+        print(f"CSV file saved as {self.merged_csv_files}")
 
-    def convert_to_GPX(self):
-        Converter(input_file=MERGED_CSV_FILES).csv_to_gpx(
+    def convert_to_GPX(self, session_data):
+        gpx_file_name = f"{session_data}.gpx"
+        Converter(input_file=self.merged_csv_files).csv_to_gpx(
             lats_colname=KEEP_COL[0],
             longs_colname=KEEP_COL[1],
-            output_file=GPX_FILE,
+            output_file=gpx_file_name,
         )
-        print(f"GPX file saved as {GPX_FILE}")
-
-    def run_all(self):
-        """Will run "create_resultant_CSV()", "clean_dataframe()", "convert_to_CSV()" and "convert_to_GPX()" """
-        self.create_resultant_CSV()
-        self.clean_dataframe()
-        self.convert_to_CSV()
-        self.convert_to_GPX()
+        print(f"GPX file saved as {gpx_file_name}")
 
 
 class PlotMap:
     """Create a map with the points from training sessions in it."""
-
-    def __init__(self):
-        self.dataframe = pd.read_csv(MERGED_CSV_FILES)
-        self.SESSION_DATE = SESSION_DATE
-        self.latitude = self.dataframe[KEEP_COL[1]]
-        self.longitude = self.dataframe[KEEP_COL[0]]
 
     def create_marker(self, row, map_obj):
         """Add markers on top of the points that are plotted on the map
@@ -150,30 +146,44 @@ class PlotMap:
         folium.Marker(location=[row["x_coord"], row["y_coord"]]).add_to(map_obj)
 
     def plot_map(self):
-        # Create a map object
-        map_obj = folium.Map(
-            location=[self.longitude.mean(), self.latitude.mean()],
-            zoom_start=12,
-            max_zoom=22,
-        )
+        for file in zip(MERGED_CSV_FILES_LOCATION, SESSION_DATE):
+            dataframe = pd.read_csv(file[0])
+            latitude = dataframe[KEEP_COL[1]]
+            longitude = dataframe[KEEP_COL[0]]
+            # Create a map object
+            map_obj = folium.Map(
+                location=[longitude.mean(), latitude.mean()],
+                zoom_start=12,
+                max_zoom=22,
+            )
 
-        # Apply the create_marker function to each 10th row
-        self.dataframe.iloc[::40].apply(self.create_marker, axis=1, args=(map_obj,))
+            # Apply the create_marker function to each 10th row
+            dataframe.iloc[::40].apply(self.create_marker, axis=1, args=(map_obj,))
 
-        # Create a 2D list from the two columns
-        coordinates = [
-            [column1, column2]
-            for column1, column2 in zip(self.longitude.tolist(), self.latitude.tolist())
-        ]
+            # Create a 2D list from the two columns
+            coordinates = [
+                [column1, column2]
+                for column1, column2 in zip(longitude.tolist(), latitude.tolist())
+            ]
 
-        # Draw line between the points
+            self.draw_map_line(map_obj, coordinates)
+
+            # Save the map to an HTML file
+            map_path = os.path.join("training_maps",f"{file[1]}.html")
+            os.makedirs(os.path.dirname(map_path), exist_ok=True)
+            map_obj.save(map_path)
+            print(f"created the training route of session {file[1]}")
+
+    def draw_map_line(self, map_obj, coordinates):
+        """Draw line between the points that are plotted on the map
+
+        Args:
+            map_obj: A Created map with Folium and Leaflet.js
+            coordinates (list [int]): 2D list with x and y coordinates for the training session
+        """
         folium.PolyLine(
             locations=coordinates, color="blue", weight=2.5, opacity=1
         ).add_to(map_obj)
-
-        # Save the map to an HTML file
-        map_obj.save(f"{self.SESSION_DATE}.html")
-        print(f"created the training route of session {self.SESSION_DATE}")
 
 
 FindCSV().extract_files_by_extension()
