0759abad455733bf18aa3bf403cfef988d2166e2
Ahmed <ahmelsamahy@gmail.com>
2023-05-15 11:10:34 +0200

    feat<>: create base for pop-up window to show training sessions

--- teleop/htm/index_f_trainingSessions.js
+++ teleop/htm/index_f_trainingSessions.js
@@ -1,7 +1,19 @@
 window.onload = function () {
     var a = document.getElementById("open_trainig_sessions_list");
+    var popup = document.getElementById("popupWindow");
     a.onclick = function () {
-        alert("I am an alert box!");
+        // alert("I am an alert box!");
+        // Show the pop-up window
+        popup.style.display = "block";
         return false;
     }
+}
+
+
+function hidePopup() {
+    // Get the pop-up window element
+    var popup = document.getElementById("popupWindow");
+
+    // Hide the pop-up window
+    popup.style.display = "none";
 }
