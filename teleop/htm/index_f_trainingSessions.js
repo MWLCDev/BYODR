974c251aaed175434925ee988891f2177b545f09
Ahmed <ahmelsamahy@gmail.com>
2023-05-17 14:23:44 +0200

    refactor<>: remove the map links in training sessions popup when closed

--- teleop/htm/index_f_trainingSessions.js
+++ teleop/htm/index_f_trainingSessions.js
@@ -2,18 +2,14 @@ window.onload = function () {
     var a = document.getElementById("open_training_sessions_list");
     var popup = document.getElementById("popupWindow");
     a.onclick = function () {
-        // alert("I am an alert box!");
-        // Show the pop-up window
         popup.style.display = "block";
         return false;
     }
 }
 
-
 function hidePopup() {
-    // Get the pop-up window element
     var popup = document.getElementById("popupWindow");
-
-    // Hide the pop-up window
     popup.style.display = "none";
+    const fileList = document.getElementById('fileList');
+    fileList.innerHTML = ''; // Clear existing file list
 }
