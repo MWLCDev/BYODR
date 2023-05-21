cfd4ffe85da981734e9b91059eb9635d54190657
Ahmed <ahmelsamahy@gmail.com>
2023-05-21 17:24:46 +0200

    feat<server.py>: return JSON response with the date and href link to JS

--- teleop/htm/server.py
+++ teleop/htm/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, jsonify
 import subprocess
+from plot_training_sessions_map.draw_training_sessions import main
 
 app = Flask(__name__)
 
@@ -17,7 +18,18 @@ def run_draw_map_python():
         subprocess.call(
             ["python", "./plot_training_sessions_map/draw_training_sessions.py"]
         )
-        return "Script executed successfully!", 200
+        result = main()  # Call main() to get the result
+        sent_list = {}
+        for x in result:
+            new_key = x
+            new_age = url_for(
+                "static",
+                filename=f"training_maps/{x}.html",
+                _external=True,
+            )
+            sent_list[new_key] = new_age
+        json_sent_list = jsonify(sent_list)
+        return json_sent_list
     except Exception as e:
         return f"Error executing the script: {str(e)}", 500
 
