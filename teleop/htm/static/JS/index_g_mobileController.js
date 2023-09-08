a64bbee6562e850d9de98e4be483d370032932c6
Ahmed Mohamed <ahmelsamahy@gmail.com>
2023-09-08 13:09:08 +0200

    feat<>: move JS from .html to .js for mobile operation scripts

--- teleop/htm/static/JS/index_g_mobileController.js
+++ teleop/htm/static/JS/index_g_mobileController.js
@@ -1,9 +1,35 @@
+let ws;
+window.addEventListener('load', () => {
+  let ws_protocol = document.location.protocol === 'https:' ? 'wss://' : 'ws://';
+  let ws_url = `${ws_protocol}${document.location.hostname}:${document.location.port}/ws/send_mobile_controller_commands`;
+  ws = new WebSocket(ws_url);
+
+  ws.onopen = function (event) {
+    console.log('WebSocket connection opened', event);
+  };
+
+  //Check the respond from the endpoint. If the user is operator or viewer 
+  // ws.onmessage = function (event) {
+  //   // Handle the received data as needed
+  //   // console.log('Message received:', event.data);
+  // };
+
+  ws.onerror = function (error) {
+    console.log('WebSocket Error:', error);
+  };
+
+  ws.onclose = function (event) {
+    console.log('WebSocket connection closed', event);
+  };
+
+  console.log('Created mobile web socket');
+});
+
 class Triangle {
-  // Move the textStyle definition to a class property to avoid redundancy
   static textStyle = new PIXI.TextStyle({
     fill: 'white',
     fontSize: 20,
-    align: 'center'
+    align: 'center',
   });
 
   constructor(direction = 'up', text = '') {
@@ -16,7 +42,6 @@ class Triangle {
       this.textObj = new PIXI.Text(text, Triangle.textStyle);
       this.textObj.anchor.set(0.5, 1);
       this.graphics.addChild(this.textObj);
-
     }
     this.updateDimensions();
     this.drawTriangle();
@@ -30,6 +55,10 @@ class Triangle {
       this.height = (this.baseWidth * Math.sqrt(3)) / 2;
     }
   }
+  /**
+   * Draw an upper or lower triangle depending on this.direction var. The triangles will have a gap between them
+   * @param {number} yOffset the position for the tip of the triangle
+   */
   drawTriangle(yOffset = 0) {
     this.graphics.clear();
     this.graphics.beginFill(0x000000);
@@ -42,15 +71,16 @@ class Triangle {
       this.vertices = [
         [(window.innerWidth - this.baseWidth) / 2, yOffset + midScreen - this.height + yOffsetAdjustment],
         [(window.innerWidth + this.baseWidth) / 2, yOffset + midScreen - this.height + yOffsetAdjustment],
-        [window.innerWidth / 2, yOffset + midScreen + yOffsetAdjustment]
+        [window.innerWidth / 2, yOffset + midScreen + yOffsetAdjustment],
       ];
     } else {
       this.vertices = [
         [(window.innerWidth - this.baseWidth) / 2, yOffset + midScreen + this.height + yOffsetAdjustment],
         [(window.innerWidth + this.baseWidth) / 2, yOffset + midScreen + this.height + yOffsetAdjustment],
-        [window.innerWidth / 2, yOffset + midScreen + yOffsetAdjustment]
+        [window.innerWidth / 2, yOffset + midScreen + yOffsetAdjustment],
       ];
       if (this.textObj) {
+        // Extra pixels is the gap the text will have with the bottom side of the triangle 
         this.textObj.position.set(window.innerWidth / 2, yOffset + midScreen + this.height - 10);
       }
     }
@@ -62,12 +92,12 @@ class Triangle {
 class Dot {
   constructor() {
     this.graphics = new PIXI.Graphics();
-    this.drawDot(0, 0);// initial position
+    this.drawDot(0, 0); // initial position
   }
 
   drawDot(x, y) {
     this.graphics.clear();
-    this.graphics.beginFill(0xffffffff); // color for the dot
+    this.graphics.beginFill(0xffffff); // color for the dot
     this.graphics.drawCircle(x, y, 9); // The radius is 9 (was requested to have diameter of 5mm === 18px)
     this.graphics.endFill();
   }
@@ -80,7 +110,18 @@ class Dot {
     this.graphics.clear();
   }
 }
-
+/**
+ * Check if a point is inside a triangle using barycentric
+ * @param {number} px Point's x-coord
+ * @param {number} py Point's Y-coord
+ * @param {number} ax Triangle's 1st edge x-coord
+ * @param {number} ay Triangle's 1st edge y-coord
+ * @param {number} bx Triangle's 2nd edge x-coord
+ * @param {number} by Triangle's 2nd edge y-coord
+ * @param {number} cx Triangle's 3rd edge x-coord
+ * @param {number} cy Triangle's 3rd edge y-coord
+ * @returns 
+ */
 function pointInsideTriangle(px, py, ax, ay, bx, by, cx, cy) {
   // Compute vectors
   const v0 = [cx - ax, cy - ay];
@@ -88,11 +129,11 @@ function pointInsideTriangle(px, py, ax, ay, bx, by, cx, cy) {
   const v2 = [px - ax, py - ay];
 
   // Compute dot products
-  const dot00 = (v0[0] * v0[0]) + (v0[1] * v0[1]);
-  const dot01 = (v0[0] * v1[0]) + (v0[1] * v1[1]);
-  const dot02 = (v0[0] * v2[0]) + (v0[1] * v2[1]);
-  const dot11 = (v1[0] * v1[0]) + (v1[1] * v1[1]);
-  const dot12 = (v1[0] * v2[0]) + (v1[1] * v2[1]);
+  const dot00 = v0[0] * v0[0] + v0[1] * v0[1];
+  const dot01 = v0[0] * v1[0] + v0[1] * v1[1];
+  const dot02 = v0[0] * v2[0] + v0[1] * v2[1];
+  const dot11 = v1[0] * v1[0] + v1[1] * v1[1];
+  const dot12 = v1[0] * v2[0] + v1[1] * v2[1];
 
   // Compute barycentric coordinates
   const invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
@@ -100,58 +141,67 @@ function pointInsideTriangle(px, py, ax, ay, bx, by, cx, cy) {
   const v = (dot00 * dot12 - dot01 * dot02) * invDenom;
 
   // Check if the point is inside the triangle
-  return (u >= 0) && (v >= 0) && (u + v < 1);
+  return u >= 0 && v >= 0 && u + v < 1;
 }
+//The starting y coord when the triangles are relocated ()
 let initialYOffset = 0;
 cursorFollowingDot = new Dot();
 let selectedTriangle = null; // null indicates no triangle is selected yet.
+let throttle_steering_json = {}; // Hold the current value for steering and throttle to be sent through the websocket
+const midScreen = window.innerHeight / 2 + initialYOffset;
 /**
  * Prints x,y and distance from the tip of the triangle to the current place of the ball.
  * @param {number} x current position of the ball (same as touch)
  * @param {*} y current position for the ball (same as touch)
  */
 function calculateDistancePercentage(x, y) {
-  const midScreen = window.innerHeight / 2 + initialYOffset;
   let distance_percentage;
 
   // Calculate the differences in x and y coordinates to get coordinates relative to the tip
   const relativeX = x - window.innerWidth / 2;
   const relativeY = y - midScreen;
 
-
   if (y < midScreen) {
-    distance_percentage = (midScreen - y) / topTriangle.height * 100;
+    distance_percentage = ((midScreen - y) / topTriangle.height) * 100;
     PrintStatistics(distance_percentage, relativeX, relativeY);
   } else {
-    distance_percentage = (y - midScreen) / bottomTriangle.height * 100;
+    distance_percentage = ((y - midScreen) / bottomTriangle.height) * 100;
     PrintStatistics(distance_percentage, relativeX, relativeY);
   }
-}
+  function PrintStatistics(speed, x, y) {
+    let shapeHeight = window.innerHeight / 4; //It is the same value as in updateDimensions()=> this.height
 
-function PrintStatistics(speed, x, y) {
-  console.log("Distance from tip:", Math.round(speed) + "%",
-    "X-coordinate:", Math.round(x),
-    "Y-coordinate:", Math.round(y));
+    // console.log("Distance from tip:", Math.round(speed) + "%",
+    //   "X-coordinate:", Math.round(x),
+    //   "Y-coordinate:", Math.round(y));
+
+    throttle_steering_json = {
+      throttle: -((y - initialYOffset) / (window.innerHeight / 4)).toFixed(3),
+      steering: Number((x / (shapeHeight / Math.sqrt(3))).toFixed(3)),
+      button_b: 1,
+    };
+    // console.log(ws)
+  }
 }
 
 /**
  * Set the value for the dot on the screen, and limit the movement to be inside the triangle the user touched first
  * @param {number} x position of the touch
- * @param {number} y position of the touch 
+ * @param {number} y position of the touch
  */
 function handleDotMove(x, y) {
-  // the triangles are divided by a mid-point. It can be referred to as the tip (the 10 px gap) 
+  // the triangles are divided by a mid-point. It can be referred to as the tip (the 10 px gap)
   const midScreen = window.innerHeight / 2 + initialYOffset;
 
   let minY, maxY, triangle;
 
   /*
-  relativeY variable represents the fraction of the distance the dot is from the tip of the triangle it is inside.
-  maxXDeviation is used to limit the movement of the ball
+relativeY variable represents the fraction of the distance the dot is from the tip of the triangle it is inside.
+maxXDeviation is used to limit the movement of the ball
 
-  if condition is used to decide which triangle is the dot currently inside of it now
-  If the dot's y coordinate is less than the mid-point, then it's inside the top triangle. Otherwise, it's inside the bottom triangle.
-  */
+if condition is used to decide which triangle is the dot currently inside of it now
+If the dot's y coordinate is less than the mid-point, then it's inside the top triangle. Otherwise, it's inside the bottom triangle.
+*/
 
   if (selectedTriangle === 'top') {
     minY = midScreen - topTriangle.height;
@@ -175,13 +225,13 @@ function handleDotMove(x, y) {
 const app = new PIXI.Application({
   width: window.innerWidth,
   height: window.innerHeight - 20,
-  backgroundColor: 0xFFFFFF,
-  sharedTicker: true
+  backgroundColor: 0xffffff,
+  sharedTicker: true,
 });
 
 // Add a 10px margin to the top and bottom of the canvas
-app.view.style.marginTop = "10px";
-app.view.style.marginBottom = "10px";
+app.view.style.marginTop = '10px';
+app.view.style.marginBottom = '10px';
 document.body.appendChild(app.view);
 
 //Creating the two triangles
@@ -203,7 +253,6 @@ function redraw(yOffset = 0) {
   app.stage.addChild(cursorFollowingDot.graphics);
 }
 
-
 window.addEventListener('resize', () => {
   app.renderer.resize(window.innerWidth, window.innerHeight);
   topTriangle.updateDimensions();
@@ -211,12 +260,11 @@ window.addEventListener('resize', () => {
   redraw();
 });
 
-function handleMove(y) {
-
+function handleTriangleMove(y) {
   const midScreen = window.innerHeight / 2;
   let yOffset = y - midScreen;
 
-  const maxOffset = midScreen + topTriangle.height;    // Maximum offset for the top triangle
+  const maxOffset = midScreen + topTriangle.height; // Maximum offset for the top triangle
   const minOffset = midScreen + bottomTriangle.height; // Minimum offset for the bottom triangle
 
   // Clamping the yOffset value to stop the triangles from crossing the screen's border
@@ -229,9 +277,9 @@ function handleMove(y) {
   redraw(yOffset);
 }
 /**
- * Second way to limit the user's interactions to be only inside the two triangles (first one is the if condition in handleMove() to limit the borders of the triangles)
+ * Second way to limit the user's interactions to be only inside the two triangles (first one is the if condition in handleTriangleMove() to limit the borders of the triangles)
  * @param {number} x the x-value for the position of touch
- * @param {number} y The y-value for the position of touch 
+ * @param {number} y The y-value for the position of touch
  * @returns If the touch was in any of the two triangles, or even out
  */
 function detectTriangle(x, y) {
@@ -244,29 +292,33 @@ function detectTriangle(x, y) {
     return 'none';
   }
 }
-
-
-
+let intervalId;
 app.view.addEventListener('touchstart', (event) => {
   initialYOffset = event.touches[0].clientY - window.innerHeight / 2; // Calculate the initial Y offset
   const detectedTriangle = detectTriangle(event.touches[0].clientX, event.touches[0].clientY);
   //if condition  to make sure it will move only if the user clicks inside one of the two triangles
   if (detectedTriangle !== 'none') {
     selectedTriangle = detectedTriangle; // Set the selected triangle
-    console.log("inside the triangles");
+    console.log('inside the triangles');
 
     // Create the dot
     cursorFollowingDot = new Dot();
     handleDotMove(event.touches[0].clientX, event.touches[0].clientY);
     app.stage.addChild(cursorFollowingDot.graphics);
+    handleTriangleMove(event.touches[0].clientY);
 
-    handleMove(event.touches[0].clientY);
     app.view.addEventListener('touchmove', onTouchMove);
+    // Arrow function to send the command through websocket 
+    intervalId = setInterval(() => {
+      if (ws && ws.readyState === WebSocket.OPEN) {
+        ws.send(JSON.stringify(throttle_steering_json));
+      } else {
+        console.error('WebSocket is not open. Unable to send data.');
+      }
+      // The interval is high so it overcomes if a user is controlling from the normal UI with a controller (PS4)
+    }, 1);
   }
 });
-
-
-
 
 app.view.addEventListener('touchend', () => {
   redraw(); // Reset triangles to their original position
@@ -278,8 +330,9 @@ app.view.addEventListener('touchend', () => {
   }
   selectedTriangle = null; // Reset the selected triangle
   app.view.removeEventListener('touchmove', onTouchMove);
+  throttle_steering_json = { steering: 0, throttle: 0 };
+  clearInterval(intervalId);
 });
-
 
 function onTouchMove(event) {
   event.preventDefault(); // Prevent scrolling while moving the triangles
@@ -289,4 +342,3 @@ function onTouchMove(event) {
     handleDotMove(event.touches[0].clientX, event.touches[0].clientY);
   }
 }
-
