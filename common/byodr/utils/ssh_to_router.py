d6c4777e758b9447110165876840a1c4f8359986
OrestisTS <118257809+OrestisTS@users.noreply.github.com>
2023-10-25 08:59:42 +0200

    feat<common/byodr/utils/ssh_to_router.py>:Add ssh to router + TODO list
    This function is able to send ssh commands to the router and extract the data into a variable for use in the python scripts
    Also added TODO list.

--- common/byodr/utils/ssh_to_router.py
+++ common/byodr/utils/ssh_to_router.py
@@ -1,13 +1,21 @@
 import paramiko
 
 
-def fetch_ssid(host, port, username, password, command):
+def get_router_data(host, port, username, password, command):
     # Create an SSH client instance
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    
     # Connect to the SSH server
     client.connect(host, port, username, password)
+
+    # Will ssh to the router, and will execute "command" in the router's cli
     stdin, stdout, stderr = client.exec_command(command)
     output = stdout.read().decode("utf-8")
     client.close()
     return output
+
+
+# TODO
+# INSTALL PARAMIKO TO THE COMS DOCKERFILE
+# GET THE IP LIST
