e4ad4267075d7612e7e64892ed74e9389c5b0e52
OrestisTS <118257809+OrestisTS@users.noreply.github.com>
2023-10-25 09:01:25 +0200

    refactor<coms/coms/app.py>:Add function ssh to router function
    Add the function that is able to extract data from the router. This function will need to send an IP table to the Teleop service.

--- coms/coms/app.py
+++ coms/coms/app.py
@@ -1,5 +1,6 @@
 import logging
 from byodr.utils.ip_getter import get_ip_number
+from byodr.utils.ssh_to_router import get_router_data
 from .server import start_server
 from .client import connect_to_server
 
@@ -14,6 +15,15 @@ if __name__ == "__main__":
 
     # Getting the 3rd digit of the IP of the local device
     local_third_ip_digit = get_ip_number()
+
+    ip_list = get_router_data(host = "192.168." + local_third_ip_digit + ".1",
+                            port = 22,
+                            username = "root",
+                            password = "Modem001",
+                            command = "arp")
+
+
+    logger.info(f"{ip_list}")
 
     # Here we statically assume that if a segment has an ip of 192.168.2.0, its the lead
     if local_third_ip_digit == "2":
