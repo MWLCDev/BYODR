825c357631c71f2227875ef1e211d444b2541220
OrestisTS <118257809+OrestisTS@users.noreply.github.com>
2023-10-30 14:42:57 +0100

    refactor<common/byodr/utils/ssh_to_router.py>: Tried to receive ip table
    The IP table by executing "arp -n" is not returned by the function. All we are getting is an empty list.

--- common/byodr/utils/ssh_to_router.py
+++ common/byodr/utils/ssh_to_router.py
@@ -1,21 +1,46 @@
 import paramiko
+import logging
 
+# Declaring the logger
+logging.basicConfig(format='%(levelname)s: %(asctime)s %(filename)s %(funcName)s %(message)s', datefmt='%Y%m%d:%H:%M:%S %p %Z')
+logging.getLogger().setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
 
 def get_router_data(host, port, username, password, command):
     # Create an SSH client instance
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     
-    # Connect to the SSH server
-    client.connect(host, port, username, password)
+    try:
+        # Connect to the SSH server
+        client.connect(host, port, username, password)
 
-    # Will ssh to the router, and will execute "command" in the router's cli
-    stdin, stdout, stderr = client.exec_command(command)
-    output = stdout.read().decode("utf-8")
-    client.close()
-    return output
+        # Execute the 'arp' command and capture the output
+        stdin, stdout, stderr = client.exec_command(command)
 
+        # Read and print the output line by line
+        output = stdout.read().decode()
+        logger.info(f"ARP Table Output: <{output}>")
+        
+        # Process the ARP output, split it into lines, and store it in a list
+        arp_table = []
+        lines = output.splitlines()
 
-# TODO
-# INSTALL PARAMIKO TO THE COMS DOCKERFILE
-# GET THE IP LIST
+        for line in lines:
+            # Skip header lines
+            if not line.startswith("IP address"):
+                columns = line.split()
+                if len(columns) == 6:
+                    ip, hw_type, flags, mac, mask, device = columns
+                    arp_table.append((ip, mac))
+
+        client.close()
+        return arp_table
+    
+    except paramiko.AuthenticationException:
+        logger.error("Authentication failed. Please check your credentials.")
+    except paramiko.SSHException as e:
+        logger.error(f"SSH connection failed: {str(e)}")
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+    return []
