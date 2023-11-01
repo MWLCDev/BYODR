93e7a88360d10fab75276f020bf251bfd388ea43
Berna <93603466+bernase@users.noreply.github.com>
2023-11-01 11:02:22 +0100

    RFC0001-SegmentCoop Getting IP list

--- common/byodr/utils/ssh_to_router.py
+++ common/byodr/utils/ssh_to_router.py
@@ -1,46 +1,39 @@
-import paramiko
 import logging
+import subprocess
 
 # Declaring the logger
 logging.basicConfig(format='%(levelname)s: %(asctime)s %(filename)s %(funcName)s %(message)s', datefmt='%Y%m%d:%H:%M:%S %p %Z')
 logging.getLogger().setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
 
-def get_router_data(host, port, username, password, command):
-    # Create an SSH client instance
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    
+
+def get_router_arp_table():
     try:
-        # Connect to the SSH server
-        client.connect(host, port, username, password)
-
-        # Execute the 'arp' command and capture the output
-        stdin, stdout, stderr = client.exec_command(command)
-
-        # Read and print the output line by line
-        output = stdout.read().decode()
-        logger.info(f"ARP Table Output: <{output}>")
+        # Read the ARP table from /proc/net/arp
+        with open('/proc/net/arp', 'r') as arp_file:
+            arp_table = arp_file.read()
         
-        # Process the ARP output, split it into lines, and store it in a list
-        arp_table = []
-        lines = output.splitlines()
-
-        for line in lines:
-            # Skip header lines
-            if not line.startswith("IP address"):
-                columns = line.split()
-                if len(columns) == 6:
-                    ip, hw_type, flags, mac, mask, device = columns
-                    arp_table.append((ip, mac))
-
-        client.close()
         return arp_table
-    
-    except paramiko.AuthenticationException:
-        logger.error("Authentication failed. Please check your credentials.")
-    except paramiko.SSHException as e:
-        logger.error(f"SSH connection failed: {str(e)}")
     except Exception as e:
         logger.error(f"An error occurred: {str(e)}")
-    return []
+        return []
+
+def get_filtered_router_arp_table(arp_table):
+    try:
+        filtered_arp_table = []
+    
+        # Split the ARP table into lines
+        arp_lines = arp_table.split('\n')
+
+        # Extract and add "IP address" and "Flags" to the filtered table which is what we need
+        for line in arp_lines:
+            columns = line.split()
+            if len(columns) >= 2:
+                ip = columns[0]
+                flags = columns[2]
+                filtered_arp_table.append({'IP address': ip, 'Flags': flags})
+    
+        return filtered_arp_table
+    except Exception as e:
+        logger.error(f"An error occurred while filtering ARP table: {str(e)}")
+        return []
--- coms/coms/app.py
+++ coms/coms/app.py
@@ -1,6 +1,6 @@
 import logging
 from byodr.utils.ip_getter import get_ip_number
-from byodr.utils.ssh_to_router import get_router_data
+from byodr.utils.ssh_to_router import get_router_arp_table, get_filtered_router_arp_table
 from .server import start_server
 from .client import connect_to_server
 
@@ -18,15 +18,17 @@ if __name__ == "__main__":
 
     while True:
 
+        arp_list = get_router_arp_table()  
+        logger.info(f"Output whole list: <{arp_list}> from 192.168.{local_third_ip_digit}.1")
 
-        ip_list = get_router_data(host = "192.168." + local_third_ip_digit + ".1",
-                                port = 22,
-                                username = "root",
-                                password = "Modem001",
-                                command = "arp -n")
+        filtered_list = get_filtered_router_arp_table(arp_list)
 
+        # Created a string for logging due to messy output i had before
+        log_string = "IP address".ljust(20) + "Flags\n"
+        for entry in filtered_list[1:]:
+            log_string += entry['IP address'].ljust(20) + entry['Flags'] + "\n"
 
-        logger.info(f"Output: <{ip_list}> from 192.168.{local_third_ip_digit}.1")
+        logger.info(f"Output filtered list:\n{log_string}from 192.168.{local_third_ip_digit}.1")
 
     # Here we statically assume that if a segment has an ip of 192.168.2.0, its the lead
     if local_third_ip_digit == "2":
