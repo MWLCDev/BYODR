3199364d6e968115c4ec5696ec8107320b7f3088
Ahmed Mahfouz <ahmelsamahy@gmail.com>
2023-12-04 11:23:30 +0100

    refactor<utils/ssh>: remove functions made by SegCooperation team
    I replaced the functions with other use that is related to them. They would need IP tables from Teleop service now

--- common/byodr/utils/ssh.py
+++ common/byodr/utils/ssh.py
@@ -60,41 +60,6 @@ class Router:
 
         print("Devices found: ", sorted_devices)
 
-    def get_router_arp_table():  ######################################################
-        try:
-            # Read the ARP table from /proc/net/arp
-            with open("/proc/net/arp", "r") as arp_file:
-                arp_table = arp_file.read()
-
-            return arp_table
-        except Exception as e:
-            logger.error(f"An error occurred: {str(e)}")
-            return []
-
-    def get_filtered_router_arp_table(
-        arp_table, last_digit_of_localIP
-    ):  ######################################################
-        try:
-            filtered_arp_table = []
-
-            # Split the ARP table into lines
-            arp_lines = arp_table.split("\n")
-            local_ip_prefix = f"192.168.{last_digit_of_localIP}."
-
-            # Extract and add "IP address" and "Flags" to the filtered table which is what we need
-            for line in arp_lines:
-                columns = line.split()
-                if len(columns) >= 2:
-                    ip = columns[0]
-                    flags = columns[2]
-                    if ip == f"{local_ip_prefix}1" or ip == f"{local_ip_prefix}2":
-                        filtered_arp_table.append({"IP address": ip, "Flags": flags})
-
-            return filtered_arp_table
-        except Exception as e:
-            logger.error(f"An error occurred while filtering ARP table: {str(e)}")
-            return []
-
     # Functions fetch_wifi_networks, parse_iwlist_output, parse_ie_data, extract_security_info all are working together
     def fetch_wifi_networks(self):
         """
