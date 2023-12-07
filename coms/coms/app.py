d8272cfe6f9dc1537be36edcbb76eff2f588f9c4
OrestisTS <118257809+OrestisTS@users.noreply.github.com>
2023-12-07 12:35:45 +0100

    refactor<coms/coms/app.py>:Remove all prints except Vehicles, less load
    Removed all loggers to remove load on the Nano.

--- coms/coms/app.py
+++ coms/coms/app.py
@@ -2,7 +2,6 @@ import logging
 import threading
 import multiprocessing
 from byodr.utils.ip_getter import get_ip_number
-from byodr.utils.ssh_to_router import get_router_arp_table, get_filtered_router_arp_table
 from byodr.utils.ipc import JSONZmqClient, JSONServerThread, ReceiverThread
 from .server import start_server
 from .client import connect_to_server
@@ -20,10 +19,11 @@ local_third_ip_digit = get_ip_number()
 
 
 def _on_receive(message):
-        logger.info(f"Data received from the Servos service, Listener: {message}.")
+    logger.info(f"Data received from the Pilot-relay service, Listener: {message}.")
 
 def _on_message(message):
-    logger.info(f"Data received from the Teleop service, Listener: {message}.")
+    # logger.info(f"Data received from the Teleop service, Listener: {message}.")
+    pass
 
 
 # Declaring the inter-service sockets
@@ -33,35 +33,22 @@ teleop_receiver = JSONServerThread(url="ipc:///byodr/teleop_to_coms.sock", event
 teleop_receiver.message_to_send = "Coms"
 teleop_receiver.add_listener(_on_message)
 
-# servos_receiver = ReceiverThread('tcp://192.168.' + local_third_ip_digit + '.32:4444', topic=b'ras/drive/velocity')
-servos_receiver = ReceiverThread('tcp://192.168.' + local_third_ip_digit + '.32:5555', topic=b'ras/drive/status')
-servos_receiver.add_listener(_on_receive)
-
-
-teleop_receiver.start()
-servos_receiver.start()
-
+velocity_receiver = ReceiverThread(url='ipc:///byodr/velocity_to_coms.sock', topic=b'ras/drive/velocity')
+velocity_receiver.add_listener(_on_receive)
 
 def main():
 
     # Getting the 3rd digit of the IP of the local device
     local_third_ip_digit = get_ip_number()
 
-
-    arp_list = get_router_arp_table()
-    filtered_list = get_filtered_router_arp_table(arp_list, local_third_ip_digit)
-
-
-    # To be able too see a nice table on logger
-    log_string = "IP address".ljust(20) + "Flags\n"
-    for entry in filtered_list:
-        log_string += entry['IP address'].ljust(20) + entry['Flags'] + "\n"
-    logger.info(f"Output filtered list:\n{log_string}from 192.168.{local_third_ip_digit}.1")
+    # Starting the receivers
+    teleop_receiver.start()
+    velocity_receiver.start()
 
     while True:
         # Setting test data to the inter-service sockets
         reply_from_pilot = pilot_publisher.call(dict(data = "Coms"))
-        logger.info(f"Message received from Pilot: {reply_from_pilot}")
+        # logger.info(f"Message received from Pilot: {reply_from_pilot}")
 
 
     # Threads that will be executing the server and client codes
