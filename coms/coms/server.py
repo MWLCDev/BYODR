e742b03f8b22a6988e5cb54956e1e5b9030e8edc
OrestisTS <118257809+OrestisTS@users.noreply.github.com>
2024-01-05 13:57:20 +0100

    feat<coms/server.py>:Add class with server functionality
    Add the server class that will wait for a client to connect, and receive data first, then send a reply.
    The timeout for the send-receive trip is 100ms.

--- coms/coms/server.py
+++ coms/coms/server.py
@@ -1,6 +1,8 @@
 import socket
 import logging
+import threading
 import time
+import numpy as np
 from byodr.utils.ssh import Nano
 nano_ip = Nano.get_ip_address()
 
@@ -11,57 +13,89 @@ log_format = "%(levelname)s: %(filename)s %(funcName)s %(message)s"
 
 
 
-# Variables for the server to start
-PORT = 1111
-IP = nano_ip
-FORMAT = "utf-8"
+class Segment_server(threading.Thread):
+
+    # Method that is called after the class is being initiated, to give it its values
+    def __init__(self, arg_server_ip, arg_server_port, arg_timeout):
+        super(Segment_server, self).__init__()
+        
+        # Giving the class the values from the class call
+        self.server_ip = arg_server_ip
+        self.server_port = arg_server_port
+        self.timeout = arg_timeout # Maybe 100ms
+        self.movement_command_received = ""
+        self.reply_to_client = "Im the server"
+
+        # The server socket that will wait for clients to connect
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((self.server_ip, self.server_port))
 
 
+    # Method that starts executing when we start an instance of the class as a thread.
+    def run(self):
 
-# Sending data from this segment to a follower segment
-# This function will be called after a follower segment connects to the server that starts at line 80
-def send_data(function_client_socket, function_client_address):
-    
+        self.server_socket.listen() # Server starts waiting for clients to connect. Blocking function
 
-    logger.info(f"[NEW CONNECTION] {function_client_address} connected.")
-    
-
-    while True:
-        try:
-            time_counter = time.perf_counter()
-
-            # Sending test string to the follower segment
-            function_client_socket.send(f"This is the lead with address {IP}:{PORT}".encode(FORMAT))
-
-            # Receiving reply from the follower segment
-            received_message = function_client_socket.recv(512).decode(FORMAT)
-            time_counter_stop = time.perf_counter()
-
-            logger.info(f"Received reply from follower. It took {(time_counter_stop-time_counter)*1000}ms")
-
-            time.sleep(2)
-
-        except ConnectionResetError:
-            logger.info("Client disconnected.")
-        except Exception as e:
-            logger.info(f"Got error trying to send to client: {e}")
+        while True:
+            try:
+                logger.info(f"[Server] Server is listening on {(self.server_ip, self.server_port)}")
+                client_socket, client_address = self.server_socket.accept() # Waiting for clients to connect. Blocking function
+                logger.info(f"[Server] {client_address} connected.")
 
 
+                # Starting actions when a client connects. 
+                self.communicate_with_client(client_socket)
 
-# Start listening for connections and passing to handler
-def start_server():
+            except Exception as e:
+                logger.error(f"[Server] Got error exception: {e}")
 
-    # Binding the IP and port to our socket
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((IP, PORT))
 
-    # Start accepting potential clients
-    server.listen()
+    # Method that first receives and then sends a reply to the client
+    def communicate_with_client(self, arg_client_socket):
 
-    # server.settimeout(1) # We set the timeout to 1 second.
-    logger.info(f"Server is listening on {(IP, PORT)}")
+        counter = 0
+        trip_time = np.array([])
+        
 
-    # Blocks execution until a client connects.
-    client_socket, client_address = server.accept()
+        while True:
+            try:
 
-    send_data(client_socket, client_address)            
+                # We set the timeout that the server will wait for data from the client
+                arg_client_socket.settimeout(self.timeout)
+
+                time_counter = time.perf_counter()
+                counter = counter + 1
+
+                # Receiving message from the client
+                self.movement_command_received = arg_client_socket.recv(512).decode("utf-8")
+                if counter == 100:
+                    logger.info(f"[Server] Received data from client: {self.movement_command_received}")
+
+                # Sending reply to the client
+                if counter == 100:
+                    logger.info(f"[Server] Sending reply to client: {self.reply_to_client}")
+                arg_client_socket.send(self.reply_to_client.encode("utf-8"))
+
+                time_counter_stop = time.perf_counter()
+
+                trip_time = np.append(trip_time, (time_counter_stop-time_counter)*1000)
+                if counter == 100:
+                    logger.info(f"[Server] Server ended 100 rounds. It took avg {np.sum(trip_time) / trip_time.size:.3f}ms")
+                    logger.info(f"[Server] Server ended 100 rounds. It took max {np.max(trip_time):.3f}ms")
+                    logger.info(f"[Server] Server ended 100 rounds. It took min {np.min(trip_time):.3f}ms")
+                    print("\n\n")
+
+                    trip_time = np.array([])
+                    counter = 0
+
+            # Catching potential exceptions and exiting the communication loop
+            except socket.timeout:
+                logger.error("[Server] 100ms passed without receiving data from the client")
+                break
+            except ConnectionResetError:
+                logger.error("[Server] Client disconnected.")
+                break
+            except Exception as e:
+                logger.error(f"[Server] Got error trying to send to client: {e}")
+                break
