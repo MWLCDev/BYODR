7cda991cc6e484f7d37ea3e0cda6e50e5ebdaea5
OrestisTS <118257809+OrestisTS@users.noreply.github.com>
2024-01-12 09:28:58 +0100

    refactor<coms/app.py>:Change the way client/server classes work
    The client server classes were completely in parallel, and all we did was start the class as a thread. Now in app.py, we can choose when to process data, when to send and receive data.

--- coms/coms/app.py
+++ coms/coms/app.py
@@ -1,10 +1,15 @@
 import logging
 import multiprocessing
+import threading
+import queue
+import socket
+import copy
 from byodr.utils.ipc import JSONPublisher, json_collector
 from byodr.utils.ssh import Nano
 nano_ip = Nano.get_ip_address()
 from .server import Segment_server
 from .client import Segment_client
+from .command_processor import process
 
 quit_event = multiprocessing.Event()
 
@@ -22,6 +27,8 @@ nano_port = 1111
 follower_ip = "192.168." + str( int(local_ip[8])+1 ) + ".100"
 lead_ip = "192.168." + str( int(local_ip[8])-1 ) + ".100"
 
+# A queue that will store messages received by the server of the segment
+msg_from_server_queue = queue.Queue(maxsize=1)
 
 
 # Declaring the inter-service sockets
@@ -35,51 +42,170 @@ teleop_receiver = json_collector(url='ipc:///byodr/teleop_to_coms.sock', topic=b
 # Other socket located on vehicles/rover/app.py/34
 vehicle_receiver = json_collector(url='ipc:///byodr/velocity_to_coms.sock', topic=b'ras/drive/velocity', event=quit_event)
 
+# Creating the segment client and server classes
+segment_server = Segment_server(local_ip, nano_port, 0.1) # The server that will wait for the lead to connect
+segment_client = Segment_client(follower_ip, nano_port, 0.1) # The client that will connect to a follower
+
+
 def main():
-    
-    # Creating the segment client and server classes
-    segment_server = Segment_server(local_ip, nano_port, 0.1) # The server that will wait for the lead to connect
-    segment_client = Segment_client(follower_ip, nano_port, 0.1) # The client that will connect to a follower
 
     # Starting the receivers
     teleop_receiver.start()
     vehicle_receiver.start()
 
-    # Starting the server and client
-    segment_server.start()
-    segment_client.start()
+    # Starting the functions that will allow the client and server of each segment to start sending and receiving data
+    client_interface_thread = threading.Thread( target=client_code )
+    server_interface_thread = threading.Thread( target=server_code )
+    client_interface_thread.start()
+    server_interface_thread.start()
+
+    # Closing the threads when the executions finish
+    client_interface_thread.join()
+    server_interface_thread.join()
+
+
+
+def client_code():
+    counter_client = 0
 
     while True:
 
-        # Receiving velocity of the motors from Vehicles
-        # logger.info(f"Velocity received: {vehicle_receiver.get()}.")
+        segment_client.connect_to_server()
 
-        # Receiving movement commands from Teleop and sending them to Pilot/app
-        movement_commands_from_teleop = teleop_receiver.get()
-        
-        # If the server of the current segment has not received anything from its lead, we forward commands from teleop
-        if segment_server.movement_command_received == "":
-            # print(f"Sending commands to pilot: {repr(segment_server.movement_command_received)}")
+        while True:
+            counter_client = counter_client + 1
 
-            # We ignore the "None" movement commands
-            if type(movement_commands_from_teleop) is dict:
+            # Receiving movement commands from Teleop and sending them to Pilot/app
+            # This block will be called by the head segment, since only the head receives commands from its own teleop
+            if local_ip == "192.168.1.100":
+                original_message = teleop_receiver.get()
 
-                # Sending the movement commands to Pilot and then the current segment's follower
-                segment_client.msg_to_send = movement_commands_from_teleop
-                coms_to_pilot_publisher.publish(movement_commands_from_teleop)
-
-
-        # If the server of this segment has received commands from its lead, we forward them instead
-        else:
-            # Sending the movement commands to Pilot and then the current segment's follower
-            if type(segment_server.movement_command_received) is dict:
+            # This block will be called by a segment that receives commands from its LD
+            else:
+                # Using get() will make the command wait for a data packet to be availabie in the queue, so that it can be retrieved
+                # Using get_nowait() will make the command get whatever is inside, and if its empty, it raises an exception (except queue.Empty)
+                try:
+                    original_message = msg_from_server_queue.get_nowait()
                 
-                # print(f"Sending commands to pilot: {segment_server.movement_command_received}")
-                segment_client.msg_to_send = segment_server.movement_command_received
-                coms_to_pilot_publisher.publish(segment_server.movement_command_received)
+                except queue.Empty:
+                    # If the queue is empty, we will wait for the queue to be full
+                    original_message = msg_from_server_queue.get()
+
+            # Making a new seperate object that has the same values as the original message
+            # Using message_to_send = original_message_to_send, creates a reference(message_to_send) to the same object in memory(original_message_to_send)
+            # We need this because if we use "message_to_send = original_message_to_send", or just use original_message_to_send everywhere
+            # changes in the value of message_to_send will be reflected on the value of original_message_to_send, thus altering the values that we get from the teleop_receiver 
+            message_to_send = copy.deepcopy(original_message)
+            # Try testing this:
+            # import copy
+
+            # original_dict = {"ValA": 1, "ValB": 2}
+
+            # # Simple assignment (creates a reference)
+            # copied_dict_ref = original_dict
+
+            # # Deep copy (creates a new object)
+            # copied_dict_deepcopy = copy.deepcopy(original_dict)
+
+            # # Modifying the original dictionary
+            # copied_dict_ref["ValA"] = 1000
+
+            # print("Original Dict:", original_dict)
+            # print("Copied Dict (Reference):", copied_dict_ref)
+            # print("Copied Dict (Deepcopy):", copied_dict_deepcopy)
+
+            # Running the above snippet gives these results:
+            # Original Dict: {'ValA': 1000, 'ValB': 2}
+            # Copied Dict (Reference): {'ValA': 1000, 'ValB': 2}
+            # Copied Dict (Deepcopy): {'ValA': 1, 'ValB': 2}
 
 
 
+            try:
+
+                ######################################################################################################
+                # Main part of the send/recv of the client
+                    
+                # Processing the command before sending it to the FL
+                process(message_to_send)
+
+                # Sending the command to our FL
+                segment_client.send_to_FL(message_to_send)
+
+                # Receiving a reply from the FL
+                reply_from_server = segment_client.recv_from_FL()
+
+                if counter_client == 200:
+                    logger.info(f"[Client] Got reply from server: {reply_from_server}")
+                    counter_client = 0
+            
+
+                ######################################################################################################
+
+            # Catching potential exceptions and attempting to reconnect each time
+            except ConnectionResetError:
+                logger.error("[Client] Server disconnected")
+                break
+                
+            except socket.timeout:
+                logger.error("[Client] 100ms passed without receiving data from the server")
+                break
+
+            except Exception as e:
+                logger.error(f"[Client] Got error during communication: {e}")
+                break
+
+
+def server_code():
+    counter_server = 0
+
+    while True:
+
+        segment_server.start_server()
+
+        while True:
+            counter_server = counter_server + 1
+
+            try:
+
+                ######################################################################################################
+                # Main part of the send/recv of the server
+
+                # Receiving movement commands from the LD
+                message_from_client = segment_server.recv_from_LD()
+
+                # Placing the message from the server in the queue
+                # Using put() will make the command wait for a free spot on the queue.
+                # Using put_nowait() will make the command add the data in the queue and raise an exception if its already full (except queue.Full)
+                try:
+                    msg_from_server_queue.put_nowait(message_from_client)
+                
+                except queue.Full:
+                    # If the queue is full, we empty it, and try to place the data again, but this time we will wait for the queue to be empty
+                    msg_from_server_queue.get()
+                    msg_from_server_queue.put(message_from_client)
+
+                if counter_server == 200:
+                    logger.info(f"[Server] Got message from client: {message_from_client}")
+                    counter_server = 0
+
+                # Sending a reply to the LD
+                segment_server.send_to_LD("I got your message")
+
+            ######################################################################################################
+
+            # Catching potential exceptions and exiting the communication loop
+            except socket.timeout:
+                logger.error("[Server] 100ms passed without receiving data from the client")
+                break
+
+            except ConnectionResetError:
+                logger.error("[Server] Client disconnected.")
+                break
+
+            except Exception as e:
+                logger.error(f"[Server] Got error during communication: {e}")
+                break
 
 
 if __name__ == "__main__":
