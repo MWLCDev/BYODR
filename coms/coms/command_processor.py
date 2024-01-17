ef41796fbce136d9145627aeb5537635608caddf
OrestisTS <118257809+OrestisTS@users.noreply.github.com>
2024-01-17 16:33:48 +0100

    refactor<coms/cmd_proc.py>:Remove if, remove steering inversing
    Moved the if statement from inside the processor file, to the app.py.
    Also remove the steering inverter, since all segments need to have the same steering

--- coms/coms/command_processor.py
+++ coms/coms/command_processor.py
@@ -10,13 +10,11 @@ log_format = "%(levelname)s: %(filename)s %(funcName)s %(message)s"
 
 def process(movement_command):
 
-    if type(movement_command) is dict:
+    # We reverse throttle
+    # Steering remains the same
+    movement_command["throttle"] = -(movement_command["throttle"])
 
-        # We reverse throttle and inverse steering
-        movement_command["throttle"] = -(movement_command["throttle"])
-        movement_command["steering"] = -(movement_command["steering"])
-
-        # Replacing the received command's timestamp with a current one
-        movement_command["time"] = timestamp()
+    # Replacing the received command's timestamp with a current one
+    movement_command["time"] = timestamp()
 
     return movement_command
