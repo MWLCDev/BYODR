1415c61ecb43560569f3a48ac68489257c74aed8
Ahmed Mahfouz <ahmelsamahy@gmail.com>
2024-01-18 15:13:13 +0100

    feat<COM>: create publisher
    it will broadcast a message within interval

--- coms/coms/app.py
+++ coms/coms/app.py
@@ -1,7 +1,8 @@
 import logging
 import multiprocessing
-import signal
+import signal, time
 from byodr.utils.ipc import JSONPublisher, json_collector
+from byodr.utils import timestamp
 
 
 # This flag starts as false
@@ -18,22 +19,34 @@ def _interrupt():
     quit_event.set()
 
 
-# Declaring the logger
+chatter = JSONPublisher(url="ipc:///byodr/coms_c.sock", topic="aav/coms/chatter")
 tel_chatter = json_collector(url="ipc:///byodr/teleop_c.sock", topic=b"aav/teleop/chatter", pop=True, event=quit_event)
 teleop_receiver = json_collector(url="ipc:///byodr/teleop_to_coms.sock", topic=b"aav/teleop/input", event=quit_event)
-logger = logging.getLogger(__name__)
 coms_to_pilot_publisher = JSONPublisher(url="ipc:///byodr/coms_to_pilot.sock", topic="aav/coms/input")
 
 
 def main():
-    teleop_receiver.start()
-    tel_chatter.start()
+    def chatter_message(cmd):
+        """Broadcast message from the current service to any service listening to it. It is a one time message"""
+        logger.info(cmd)
+        chatter.publish(dict(time=timestamp(), command=cmd))
+
+    threads = [teleop_receiver, tel_chatter]
+    [t.start() for t in threads]
     while not quit_event.is_set():
+        # Creating a message
+        chatter_message("Your command here")
+
         tel_data = tel_chatter.get()
         if tel_data:
             logger.info(tel_data["command"]["robot_config"])
 
+            # Publishing data in every iteration
+        time.sleep(1)
         coms_to_pilot_publisher.publish(teleop_receiver.get())
+
+    logger.info("Waiting on threads to stop.")
+    [t.join() for t in threads]
 
     return 0
 
