b16ff9fae86aa85dbbcb8cf31dc51dc6150fef8f
Ahmed Mahfouz <ahmelsamahy@gmail.com>
2024-02-27 12:16:56 +0100

    refactor<mobile_controller>: use the new directory for files
    All the JS files have shorter directory path

--- teleop/htm/static/JS/mobileController/mobileController_c_logic.js
+++ teleop/htm/static/JS/mobileController/mobileController_c_logic.js
@@ -1,7 +1,7 @@
 
-import { topTriangle, bottomTriangle } from "/JS/mobileController/mobileController_b_shape_triangle.js"
-import CTRL_STAT from '/JS/mobileController/mobileController_z_state.js';
-import { redraw } from '/JS/mobileController/mobileController_d_pixi.js';
+import { topTriangle, bottomTriangle } from "./mobileController_b_shape_triangle.js"
+import CTRL_STAT from './mobileController_z_state.js';
+import { redraw } from './mobileController_d_pixi.js';
 
 function initializeWS() {
   let WSprotocol = document.location.protocol === 'https:' ? 'wss://' : 'ws://';
