fe7a92c00b7771339a719dbce3f55ce5b8fab44a
Ahmed Mahfouz <ahmelsamahy@gmail.com>
2024-04-19 17:40:17 +0200

    feat<pil/relay> refactor some code parts

--- pilot/pilot/relay.py
+++ pilot/pilot/relay.py
@@ -80,19 +80,13 @@ class NoopMonitoringRelay(AbstractRelay):
 
 
 class RealMonitoringRelay(AbstractRelay):
-    def __init__(
-        self, relay, client_factory=None, status_factory=None, config_dir=os.getcwd()
-    ):
+    def __init__(self, relay, client_factory=None, status_factory=None, config_dir=os.getcwd()):
         super(RealMonitoringRelay, self).__init__()
         self._relay = relay
         self._config_dir = config_dir
         self._integrity = MessageStreamProtocol(max_age_ms=200, max_delay_ms=250)
-        self._status_factory = (
-            StatusReceiverThreadFactory() if status_factory is None else status_factory
-        )
-        self._client_factory = (
-            PiClientFactory() if client_factory is None else client_factory
-        )
+        self._status_factory = StatusReceiverThreadFactory() if status_factory is None else status_factory
+        self._client_factory = PiClientFactory() if client_factory is None else client_factory
         self._relay_closed_calltrace = collections.deque(maxlen=1)
         self._patience_micro = 100.0
         self._config_hash = -1
@@ -100,12 +94,11 @@ class RealMonitoringRelay(AbstractRelay):
         self._pi_client = None
         self._pi_status = None
         self._servo_config = None
+        self.n_violations = 0
 
     def _send_config(self, data):
         if self._pi_client is not None and data is not None:
-            self._pi_client.call(
-                dict(time=timestamp(), method="ras/driver/config", data=data)
-            )
+            self._pi_client.call(dict(time=timestamp(), method="ras/driver/config", data=data))
 
     def _send_drive(self, throttle=0.0, steering=0.0, reverse_gear=False, wakeup=False):
         if self._pi_client is not None:
@@ -113,34 +106,19 @@ class RealMonitoringRelay(AbstractRelay):
             steering = max(-1.0, min(1.0, steering))
             _reverse = 1 if reverse_gear else 0
             _wakeup = 1 if wakeup else 0
-            self._pi_client.call(
-                dict(
-                    time=timestamp(),
-                    method="ras/servo/drive",
-                    data=dict(
-                        steering=steering,
-                        throttle=throttle,
-                        reverse=_reverse,
-                        wakeup=_wakeup,
-                    ),
-                )
-            )
+            self._pi_client.call(dict(time=timestamp(), method="ras/servo/drive", data=dict(steering=steering, throttle=throttle, reverse=_reverse, wakeup=_wakeup)))
 
     def _drive(self, pilot, teleop):
         pi_status = None if self._pi_status is None else self._pi_status.pop_latest()
         if pi_status is not None and not bool(pi_status.get("configured")):
+            print(self._servo_config)
             self._send_config(self._servo_config)
         if pilot is None:
             self._send_drive()
         else:
             _reverse = teleop and teleop.get("arrow_down", 0)
             _wakeup = teleop and teleop.get("button_b", 0)
-            self._send_drive(
-                steering=pilot.get("steering"),
-                throttle=pilot.get("throttle"),
-                reverse_gear=_reverse,
-                wakeup=_wakeup,
-            )
+            self._send_drive(steering=pilot.get("steering"), throttle=pilot.get("throttle"), reverse_gear=_reverse, wakeup=_wakeup)
 
     def _config(self):
         parser = SafeConfigParser()
@@ -161,8 +139,8 @@ class RealMonitoringRelay(AbstractRelay):
     def _reboot(self):
         errors = []
         _config = self._config()
-        self._patience_micro = parse_option('patience.ms', int, 100, errors, **_config) * 1000.
-        _pi_uri = parse_option('ras.master.uri', str, 'tcp://192.168.1.32', errors, **_config)
+        self._patience_micro = parse_option("patience.ms", int, 100, errors, **_config) * 1000.0
+        _pi_uri = parse_option("ras.master.uri", str, "tcp://192.168.1.32", errors, **_config)
         _pi_uri = f"tcp://{_pi_uri}"
         # Stopping the sockets that handle communication with the Pi
         if self._pi_client is not None:
@@ -175,15 +153,10 @@ class RealMonitoringRelay(AbstractRelay):
         self._pi_status = self._status_factory.create(_pi_uri)
         self._pi_status.add_listener(self._on_receive)
         self._pi_status.start()
-        _steering_offset = parse_option(
-            "ras.driver.steering.offset", float, 0.0, errors, **_config
+        _steering_offset = parse_option("ras.driver.steering.offset", float, 0.0, errors, **_config)
+        _motor_scale = parse_option("ras.driver.motor.scale", float, 1.0, errors, **_config)
         _motor_alternate = parse_option("ras.driver.motor.alternate", bool, False, errors, **_config)
         self._servo_config = dict(app_version=2, steering_offset=_steering_offset, motor_scale=_motor_scale, motor_alternate=_motor_alternate)
-            "ras.driver.motor.scale", float, 1.0, errors, **_config
-        )
-        self._servo_config = dict(
-            app_version=2, steering_offset=_steering_offset, motor_scale=_motor_scale
-        )
         self._integrity.reset()
         self._send_config(self._servo_config)
         return errors
@@ -195,10 +168,7 @@ class RealMonitoringRelay(AbstractRelay):
     def _close_relay(self):
         # In normal operation the relay is constantly asked to close.
         now = timestamp()
-        if (
-            len(self._relay_closed_calltrace) == 0
-            or (now - self._relay_closed_calltrace[-1]) > 30 * 1e6
-        ):
+        if len(self._relay_closed_calltrace) == 0 or (now - self._relay_closed_calltrace[-1]) > 30 * 1e6:
             self._relay.close()
             self._relay_closed_calltrace.append(now)
 
@@ -213,14 +183,14 @@ class RealMonitoringRelay(AbstractRelay):
         # Always consume the latest commands.
         c_pilot = self._latest_or_none(pilot, patience=self._patience_micro)
         c_teleop = self._latest_or_none(teleop, patience=self._patience_micro)
-        n_violations = self._integrity.check()
-        if n_violations < -5:
+        self.n_violations = self._integrity.check()
+        if self.n_violations < -5:
             self._close_relay()
             self._drive(c_pilot, c_teleop)
-        elif n_violations > 200:
+        elif self.n_violations > 200:
             # ZeroMQ ipc over tcp does not allow connection timeouts to be set - while the timeout is too high.
             self._reboot()  # Resets the protocol.
-        elif n_violations > 5:
+        elif self.n_violations > 5:
             self._open_relay()
             self._drive(None, None)
         else:
@@ -232,9 +202,7 @@ def main():
     subparsers = parser.add_subparsers(help="Methods.")
 
     parser_a = subparsers.add_parser("exec", help="Open or close the relay.")
-    parser_a.add_argument(
-        "--cmd", type=str, required=True, help="Open or close the relay."
-    )
+    parser_a.add_argument("--cmd", type=str, required=True, help="Open or close the relay.")
     parser_a.set_defaults(func=execute)
 
     args = parser.parse_args()
