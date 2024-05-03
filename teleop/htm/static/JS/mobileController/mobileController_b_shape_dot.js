5a3bbe376de37613f374fcfe8d78c283ce1cc38f
OrestisTS <118257809+OrestisTS@users.noreply.github.com>
2024-05-03 16:26:41 +0200

    refactor<b_Shape_dot.js>:Remake

---
+++ teleop/htm/static/JS/mobileController/mobileController_b_shape_dot.js
@@ -0,0 +1,33 @@
+class Dot {
+  constructor() {
+    this.graphics = new PIXI.Graphics();
+    this.drawDot(0, 0); // initial position
+  }
+
+  drawDot(x, y) {
+    this.graphics.clear();
+    this.graphics.beginFill(0xffffff); // color for the dot
+    this.graphics.drawCircle(x, y, 18); // The radius is 18 (was requested to have diameter of 10mm === 36px)
+    this.graphics.endFill();
+  }
+
+  setPosition(x, y) {
+    this.drawDot(x, y);
+  }
+
+  remove() {
+    this.graphics.clear();
+  }
+
+  hide() {
+    this.graphics.alpha = 0;
+  }
+
+  show() {
+    this.graphics.alpha = 1;
+  }
+}
+
+const cursorFollowingDot = new Dot();
+
+export { cursorFollowingDot };
