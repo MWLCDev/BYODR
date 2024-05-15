563f8bec952547484ef42d86e9690fed78d6017f
mkbielaw <bielawski.pf@gmail.com>
2024-05-15 11:35:26 +0200

    refactor<following>:Added different stages of turning the robot
    + minor refactors

--- following/app.py
+++ following/app.py
@@ -11,9 +11,12 @@ from byodr.utils.option import parse_option
 
 # Constants
 SCREEN_CENTER = 320
-START_HEIGHT = 470
-UNSAFE_HEIGHT = 475
-MAX_HUMAN_ABSENCE_FRAMES = 3
+CENTER_OFFSET = 0
+SECOND_OFFSET = 100
+THIRD_OFFSET = 70
+START_HEIGHT = 340
+UNSAFE_HEIGHT = 360
+MAX_HUMAN_ABSENCE_FRAMES = 10
 MIN_CLEAR_PATH_FRAMES = 3
 SMOOTH_CONTROL_STEP = 0.1
 
@@ -73,7 +76,9 @@ class FollowingController:
             try:
                 if request['following'] == "Stop Following":  # Sending no movement if following stopped
                     self.logger.info("Stopping Following")
-                    self.publish_command(0, 0)
+                    self.current_throttle = 0
+                    self.current_steering = 0
+                    self.publish_command(self.current_throttle, self.current_steering)
                     return
             except:
                 pass
@@ -84,21 +89,26 @@ class FollowingController:
             self.current_throttle += SMOOTH_CONTROL_STEP
         else:
             self.current_throttle = target_throttle                                         # Passing without smoothing if the difference is too small
-        if abs(self.current_steering) <= abs(target_steering) - SMOOTH_CONTROL_STEP:        # Steering can be negative or positive
-            self.current_steering += math.copysign(SMOOTH_CONTROL_STEP, target_steering)    # Making sure steering has the correct sign
-        else:
-            self.current_steering = target_steering
-        # self.current_steering = target_steering # steering is not being smoothed
+        # if abs(self.current_steering) <= abs(target_steering) - SMOOTH_CONTROL_STEP:        # Steering can be negative or positive
+        #     self.current_steering += math.copysign(SMOOTH_CONTROL_STEP, target_steering)    # Making sure steering has the correct sign
+        # else:
+        #     self.current_steering = target_steering
+        self.current_steering = target_steering # steering is not being smoothed
 
 
     def decide_control(self, boxes):
         if not boxes.xyxy.size:                                                             # No people detected in the frame
             self.no_human_counter += 1
             self.logger.info(f"No person detected for: {self.no_human_counter} frames")
-            try:
-                return throttle, steering                                                   # Passing the previous control command if it exists
-            except:
-                return 0, 0
+            if self.no_human_counter >= MAX_HUMAN_ABSENCE_FRAMES:
+                self.current_throttle = 0
+                self.current_steering = 0
+                return self.current_throttle, self.current_steering
+            else:
+                try:
+                    return self.current_throttle, self.current_steering                                                   # Passing the previous control command if it exists
+                except:
+                    return 0, 0
 
         throttle, steering = 0, 0                   # No movement by default
         self.no_human_counter = 0                   # Resetting the counter if a person is detected
@@ -123,41 +133,53 @@ class FollowingController:
             
             # Smaller bbox height means the detected person is further away from the camera
             if box_height <= START_HEIGHT:                                  # Starting movement if person is far enough
-                throttle = max(0, min(1, ((-(0.008) * box_height) + 3.96))) # 0.2 at 450p; 1 at 350p height
+                throttle = max(0.2, min(1, ((-(0.01) * box_height) + 3.6))) # 0.2 at 320p height; 1 at 240p height
 
             # Keeping the user in the center of the camera view
-            if box_center < SCREEN_CENTER:      # left = negative steering
-                steering = min(0, max(-0.55, (0.0025 * box_center - 0.8)))  # 0 at 320p; 0.55 (max) at 100p
-                steering = steering*(1.2-throttle)                    # Max steering is scaled down proportionally to the throttle value, max steering = 0.11 at throttle = 1
+            if box_center < (SCREEN_CENTER - CENTER_OFFSET):      # left = negative steering
+                # steering = min(0, max(-0.6, (0.003 * box_center - 0.96)))  # 0 at 320p; 0.6 (max) at 120p
+                steering = min(0, max(-0.1, (0.001 * box_center - 0.32)))  # 0 at 320p; 0.1 at 220p
+                if box_center < (SCREEN_CENTER - CENTER_OFFSET - SECOND_OFFSET):
+                    steering = min(-0.1, max(-0.7, (0.004 * box_center - 0.98)))
+                # steering = steering*(1.2-throttle)                        # Max steering is scaled down proportionally to the throttle value, max steering = 0.16 at throttle = 1
+                steering = steering*(1.15-throttle*0.75)                    # Max steering is scaled down proportionally to the throttle value, max steering = 0.32 at throttle = 1
                 if throttle == 0:                                           # Turning in place
-                    throttle = abs(steering)/1.2                            # Scaling back
+                    throttle = (abs(steering)/1.15)                     # Scaling back
                     steering = -1                                           # Max steering, turning speed determined by throttle
-            elif box_center > SCREEN_CENTER:    # right = positive steering
-                steering = max(0, min(0.55, (0.0025 * box_center - 0.8)))   # 0 at 320p; 0.55 (max) at 540p
-                steering = steering*(1.2-throttle)
+            elif box_center > (SCREEN_CENTER + CENTER_OFFSET):    # right = positive steering
+                # steering = max(0, min(0.6, (0.003 * box_center - 0.96)))   # 0 at 320p; 0.6 (max) at 520p (values x2 for 420p)
+                steering = max(0, min(0.1, (0.001 * box_center - 0.32)))   # 0 at 320p; 0.1 at 420p
+                if box_center > (SCREEN_CENTER + CENTER_OFFSET + SECOND_OFFSET):
+                    steering = max(0.1, min(0.70, (0.004 * box_center - 1.58)))
+                # steering = steering*(1.2-throttle)                    # Max steering is scaled down proportionally to the throttle value, max steering = 0.16 at throttle = 1
+                steering = steering*(1.15-throttle*0.75)                # Max steering is scaled down proportionally to the throttle value, max steering = 0.32 at throttle = 1
                 if throttle == 0:
-                    throttle = steering/1.2
+                    throttle = (steering/1.15)*0.8
                     steering = 1
+            
+            if abs(box_center - SCREEN_CENTER) >= (SCREEN_CENTER - THIRD_OFFSET) and box_height <= START_HEIGHT:
+                self.logger.info("Operator on the far side")
+                steering = steering/(1.2-throttle)*0.8
 
-        if self.clear_path <= MIN_CLEAR_PATH_FRAMES or self.no_human_counter >= MAX_HUMAN_ABSENCE_FRAMES:
-            self.logger.info(f"Path obstructed or cannot see operator") # No movement if too few frames with clear path passed or too many frames without any detection
+        if self.clear_path <= MIN_CLEAR_PATH_FRAMES:
+            self.logger.info(f"Path obstructed") # No movement if too few frames with clear path passed or too many frames without any detection
             throttle = 0
         self.smooth_controls(throttle, steering)                        # Smoothing the movement if the commands are immediately large
         return self.current_throttle, self.current_steering
 
     def run(self):
-        self.publish_command(0, 0)  # Initializing with safe values
+        self.publish_command(self.current_throttle, self.current_steering)  # Initializing with safe values
         self.logger.info("Following ready to start")
         errors = []
         _config = self.config
         stream_uri = parse_option('ras.master.uri', str, '192.168.1.32', errors, **_config)
-        stream_uri = f"rtsp://user1:HaikuPlot876@{stream_uri[:-2]}65:554/Streaming/Channels/103" # Setting dynamic URI of the stream
+        stream_uri = f"rtsp://user1:HaikuPlot876@{stream_uri[:-2]}64:554/Streaming/Channels/103" # Setting dynamic URI of the stream
         while True:
             request = self.teleop.get()                         # Checking for requests to start following
             try:
                 if request['following'] == "Start Following":
                     self.logger.info("Loading Yolov8 model")
-                    results = self.model.track(source=stream_uri, classes=0, stream=True, conf=0.4, persist=True, verbose=False) # Image recognition with assigning IDs to objects
+                    results = self.model.track(source=stream_uri, classes=0, stream=True, conf=0.4, persist=True, verbose=True) # Image recognition with assigning IDs to objects
                     self.control_logic(results)                 # Calculating the control commands based on the model results
             except:
                 pass
@@ -165,5 +187,5 @@ class FollowingController:
 
 
 if __name__ == "__main__":
-    controller = FollowingController("320_20k.pt")
+    controller = FollowingController("480_20k.pt")
     controller.run()
