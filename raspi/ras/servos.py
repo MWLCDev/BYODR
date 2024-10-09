fb2743441826495d1ba60adc52dd64801220b77e
Ahmed Mahfouz <ahmelsamahy@gmail.com>
2024-10-09 12:38:34 +0200

    feat<servos> implement gpio and split main application into config file class
    As the main application was doing lots of things at the same time. I will reduce it later and change name for it

--- raspi/ras/servos.py
+++ raspi/ras/servos.py
@@ -11,16 +11,19 @@ import subprocess
 import time
 from abc import ABC, abstractmethod
 from configparser import ConfigParser
+
 import numpy as np
 from byodr.utils import Application, timestamp
 from byodr.utils.ipc import JSONPublisher, JSONServerThread
 from byodr.utils.option import parse_option
 from byodr.utils.protocol import MessageStreamProtocol
+from byodr.utils.usbrelay import SearchUsbRelayFactory
 from BYODR.utils.gpio_relay import ThreadSafePi4GpioRelay
+
 from .core import CommandHistory, HallOdometer, VESCDrive
 
 logger = logging.getLogger(__name__)
-log_format = "%(levelname)s: %(asctime)s %(filename)s %(funcName)s %(message)s"
+log_format = "%(levelname)s: %(asctime)s %(filename)s:%(lineno)d %(funcName)s %(threadName)s %(message)s"
 
 signal.signal(signal.SIGINT, lambda sig, frame: _interrupt())
 signal.signal(signal.SIGTERM, lambda sig, frame: _interrupt())
@@ -75,6 +78,58 @@ class AbstractDriver(ABC):
     @abstractmethod
     def quit(self):
         raise NotImplementedError()
+
+
+class ConfigFiles:
+    def __init__(self, config_dir):
+        self._config_dir = config_dir
+        self.__set_parsers()
+
+    def __set_parsers(self):
+        self.servos_config_dir = os.path.join(self._config_dir, "driver.ini")
+        self.servos_config_parser = ConfigParser()
+        self.servos_config_parser.read(self.servos_config_dir)
+
+    def check_configuration_files(self):
+        """Checks if the configuration file exists, if not, creates it from the template."""
+        config_file = "driver.ini"
+        template_file_path = "ras/driver.template"
+
+        if not os.path.exists(self.servos_config_dir):
+            shutil.copyfile(template_file_path, self.servos_config_dir)
+            logger.info("Created {} from template at {}".format(config_file, self.servos_config_dir))
+
+        self._verify_and_add_missing_keys(self.servos_config_dir, template_file_path)
+
+    def read_configuration(self):
+        parser = ConfigParser()
+        parser.read(self.servos_config_dir)
+        servos_file_arguments = {}
+        if parser.has_section("driver"):
+            servos_file_arguments.update(dict(parser.items("driver")))
+        if parser.has_section("odometer"):
+            servos_file_arguments.update(dict(parser.items("odometer")))
+        return servos_file_arguments
+
+    def _verify_and_add_missing_keys(self, ini_file, template_file):
+        config = ConfigParser()
+        template_config = ConfigParser()
+
+        config.read(ini_file)
+        template_config.read(template_file)
+
+        # Loop through each section and key in the template
+        for section in template_config.sections():
+            if not config.has_section(section):
+                config.add_section(section)
+            for key, value in template_config.items(section):
+                if not config.has_option(section, key):
+                    config.set(section, key, value)
+                    logger.info("Added missing key '{}' in section '[{}]' to {}".format(key, section, ini_file))
+
+        # Save changes to the ini file if any modifications have been made
+        with open(ini_file, "w") as configfile:
+            config.write(configfile)
 
 
 class DualVescDriver(AbstractDriver):
@@ -167,31 +222,36 @@ class DualVescDriver(AbstractDriver):
 
 
 class MainApplication(Application):
-    def __init__(self, event, config_file, relay, hz, test_mode=False):
+    def __init__(self, event, config_dir, hz, test_mode=False):
         super(MainApplication, self).__init__(run_hz=hz, quit_event=event)
         self.test_mode = test_mode
-        self.config_file_dir = config_file
-        self.relay = relay
+        self.config_dir = config_dir  # Changed to config_dir for consistency
+        self.relay = None
         self._integrity = MessageStreamProtocol(max_age_ms=100, max_delay_ms=100)
         self._cmd_history = CommandHistory(hz=hz)
         self._config_queue = collections.deque(maxlen=1)
         self._drive_queue = collections.deque(maxlen=4)
-        self._last_drive_command = None  # Store the last drive command
 
+        self._last_drive_command = None
         self._chassis = None
         self.platform = None
         self.publisher = None
-        self._odometer = None  
+        self._odometer = None
+
+        self.config_files = ConfigFiles(self.config_dir)
+        self._check_relay_type()
+        self.setup_components()
 
     def setup_components(self):
-        # Check configuration files and update, read configuration, then setup odometer and chassis
-        self.check_configuration_files()
-        kwargs = self.read_configuration()
+        # Use ConfigFiles instance to check and read configurations
+        self.config_files.check_configuration_files()
+        kwargs = self.config_files.read_configuration()
+
         self._odometer = HallOdometer(**kwargs)
 
         drive_type = kwargs.get("drive.type", "unknown")
         if drive_type == "vesc_dual":
-            self._chassis = DualVescDriver(self.relay, self.config_file_dir, **kwargs)
+            self._chassis = DualVescDriver(self.relay, self.config_dir, **kwargs)
         else:
             raise AssertionError("Unknown drive type '{}'.".format(drive_type))
 
@@ -200,46 +260,25 @@ class MainApplication(Application):
         self._cmd_history.reset()
         self._odometer.setup()
 
-    def check_configuration_files(self):
-        """Checks if the configuration file exists, if not, creates it from the template."""
-        config_file = "driver.ini"
-        template_file_path = "ras/driver.template"
-
-        if not os.path.exists(self.config_file_dir):
-            shutil.copyfile(template_file_path, self.config_file_dir)
-            logger.info("Created {} from template at {}".format(config_file, self.config_file_dir))
-
-        self._verify_and_add_missing_keys(self.config_file_dir, template_file_path)
-
-    def read_configuration(self):
-        parser = ConfigParser()
-        parser.read(self.config_file_dir)
-        kwargs = {}
-        if parser.has_section("driver"):
-            kwargs.update(dict(parser.items("driver")))
-        if parser.has_section("odometer"):
-            kwargs.update(dict(parser.items("odometer")))
-        return kwargs
-
-    def _verify_and_add_missing_keys(self, ini_file, template_file):
-        config = ConfigParser()
-        template_config = ConfigParser()
-
-        config.read(ini_file)
-        template_config.read(template_file)
-
-        # Loop through each section and key in the template
-        for section in template_config.sections():
-            if not config.has_section(section):
-                config.add_section(section)
-            for key, value in template_config.items(section):
-                if not config.has_option(section, key):
-                    config.set(section, key, value)
-                    logger.info("Added missing key '{}' in section '[{}]' to {}".format(key, section, ini_file))
-
-        # Save changes to the ini file if any modifications have been made
-        with open(ini_file, "w") as configfile:
-            config.write(configfile)
+    def _check_relay_type(self):
+        """Decide on the relay type based on configuration and initialize it."""
+        try:
+            # Use ConfigFiles to read the configuration
+            kwargs = self.config_files.read_configuration()
+            gpio_relay = kwargs.get("driver.gpio_relay", "false").strip().lower() == "true"
+            if gpio_relay:
+                self.relay = ThreadSafePi4GpioRelay()
+                logger.info("Initialized GPIO Relay")
+            else:
+                self.relay = SearchUsbRelayFactory().get_relay()
+                logger.info("Initialized USB Relay")
+                assert self.relay.is_attached(), "The relay device is not attached."
+        except AssertionError as e:
+            logger.error("Relay initialization error: %s", e)
+            self.quit()
+        except Exception as e:
+            logger.error("Unexpected error during relay type checking: %s", e)
+            self.quit()
 
     def _pop_config(self):
         return self._config_queue.popleft() if bool(self._config_queue) else None
@@ -309,18 +348,12 @@ def main():
     parser.add_argument("--config", type=str, default="/config/driver.ini", help="Configuration file.")
     args = parser.parse_args()
 
-    config_file = args.config
-
-    _relay = ThreadSafePi4GpioRelay()
     ras_dynamic_ip = subprocess.check_output("hostname -I | awk '{for (i=1; i<=NF; i++) if ($i ~ /^192\\.168\\./) print $i}'", shell=True).decode().strip().split()[0]
 
     try:
-
-        application = MainApplication(quit_event, config_file, relay=_relay, hz=50)
-
+        application = MainApplication(quit_event, args.config, hz=50)
         application.publisher = JSONPublisher(url="tcp://{}:5555".format(ras_dynamic_ip), topic="ras/drive/status")
         application.platform = JSONServerThread(url="tcp://{}:5550".format(ras_dynamic_ip), event=quit_event, receive_timeout_ms=50)
-        application.setup_components()  # Set up components including reading the config
 
         threads = [application.platform]
         if quit_event.is_set():
@@ -332,7 +365,7 @@ def main():
         logger.info("Waiting on threads to stop.")
         [t.join() for t in threads]
     finally:
-        _relay.open()
+        application.relay.open()
 
     while not quit_event.is_set():
         time.sleep(1)
